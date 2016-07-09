d5aa9992f7e6c9b1fbd747daf7f42a8e3e2aeb3a
Rene Dohmen <rene@formatics.nl>
2016-07-09 23:58:24 +0200

    Contact form

--- blog/models.py
+++ blog/models.py
@@ -94,8 +94,8 @@ class BlogIndexPage(Page):
                 blogs = paginator.page(paginator.num_pages)
 
         context['tags'] = Tag.objects.all()
-        context['categories'] = None
-
+        context['categories'] = BlogCategory.objects.all()
+        context['number_of_blog_posts'] = BlogPage.objects.count() # Todo: update with tag
         context['blogs'] = blogs
         context['category'] = category
         context['tag'] = tag
--- home/models.py
+++ home/models.py
@@ -1,14 +1,34 @@
 from __future__ import unicode_literals
 
-from django.db import models
-
-from wagtail.wagtailcore.models import Page
+from modelcluster.fields import ParentalKey
 from wagtail.wagtailcore.fields import RichTextField
-from wagtail.wagtailadmin.edit_handlers import FieldPanel
+from wagtail.wagtailcore.models import Page
+from wagtail.wagtailforms.models import AbstractEmailForm, AbstractFormField
+
+from wagtail.wagtailadmin.edit_handlers import (FieldPanel, InlinePanel, MultiFieldPanel)
+
 
 class HomePage(Page):
     body = RichTextField(blank=True)
 
     content_panels = Page.content_panels + [
         FieldPanel('body', classname="full")
+    ]
+
+class FormField(AbstractFormField):
+    page = ParentalKey('FormPage', related_name='form_fields')
+
+class FormPage(AbstractEmailForm):
+    intro = RichTextField(blank=True)
+    thank_you_text = RichTextField(blank=True)
+
+    content_panels = AbstractEmailForm.content_panels + [
+        FieldPanel('intro', classname="full"),
+        InlinePanel('form_fields', label="Form fields"),
+        FieldPanel('thank_you_text', classname="full"),
+        MultiFieldPanel([
+            FieldPanel('to_address', classname="full"),
+            FieldPanel('from_address', classname="full"),
+            FieldPanel('subject', classname="full"),
+        ], "Email")
     ]
