d67a8d86a595afa6b39b2054a95d08f42ee95bea
Rene Dohmen <acidjunk@gmail.com>
2016-10-04 11:17:13 +0200

    Added LICENSE pre amble to relevant python parts

--- fundraiser/wagtail_hooks.py
+++ fundraiser/wagtail_hooks.py
@@ -2,8 +2,23 @@ from wagtail.contrib.modeladmin.options import (
     ModelAdmin, ModelAdminGroup, modeladmin_register)
 from .models import (
     Order, ProductPage, ProductCategory, ProjectPage, ProjectCategory)
+"""
+Funder: a Django/Wagtail based web application to facilitate online donations.
+Copyright (C) 2016 R. Dohmen <acidjunk@gmail.com>
 
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
 class ProductPageModelAdmin(ModelAdmin):
     model = ProductPage
     menu_icon = 'doc-full-inverse'  # change as required
