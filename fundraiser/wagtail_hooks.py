40dfcd2f98707ae4b357b7a739b2aa32114ad6ae
acidjunk <rene.dohmen1@kpn.com>
2017-01-14 01:24:09 +0100

    Small fixes

--- fundraiser/wagtail_hooks.py
+++ fundraiser/wagtail_hooks.py
@@ -1,7 +1,7 @@
 from wagtail.contrib.modeladmin.options import (
     ModelAdmin, ModelAdminGroup, modeladmin_register)
 from .models import (
-    Order, ProductPage, ProductCategory, ProjectPage, ProjectCategory)
+    Order, ProductPage, ProjectPage)
 """
 Funder: a Django/Wagtail based web application to facilitate online donations.
 Copyright (C) 2016 R. Dohmen <acidjunk@gmail.com>
@@ -23,7 +23,6 @@ class ProductPageModelAdmin(ModelAdmin):
     model = ProductPage
     menu_icon = 'doc-full-inverse'  # change as required
     list_display = ('title', 'organisation', 'stock', 'prize', 'live')
-    # list_filter = ('title', 'organisation', 'tags', 'live')
     search_fields = ('title',)
 
 
@@ -31,9 +30,7 @@ class ProjectPageModelAdmin(ModelAdmin):
     model = ProjectPage
     menu_icon = 'doc-full-inverse'  # change as required
     list_display = ('title', 'organisation', 'teaser', 'amount_needed', 'amount_raised', 'live')
-    # list_filter = ('live', 'organisation', 'tags')
     search_fields = ('title',)
-
 
 class OrderModelAdmin(ModelAdmin):
     model = Order
@@ -44,22 +41,10 @@ class OrderModelAdmin(ModelAdmin):
     list_filter = ('billing_name', 'billing_company', 'anonymous')
     search_fields = ('title',)
 
-
-# class MySnippetModelAdmin(ModelAdmin):
-#     model = MySnippetModel
-#     menu_label = 'Snippet Model'  # ditch this to use verbose_name_plural from model
-#     menu_icon = 'snippet'  # change as required
-#     list_display = ('title', 'example_field2', 'example_field3')
-#     list_filter = (example_field2', 'example_field3')
-#     search_fields = ('title',)
-
-
-class MyModelAdminGroup(ModelAdminGroup):
+class FundRaiserAdminGroup(ModelAdminGroup):
     menu_label = 'Fundraiser'
     menu_icon = 'folder-open-inverse'  # change as required
-    menu_order = 200  # will put in 3rd place (000 being 1st, 100 2nd)
+    menu_order = 201  # will put in 4rd place (000 being 1st, 100 2nd)
     items = (OrderModelAdmin, ProductPageModelAdmin, ProjectPageModelAdmin)
 
-# When using a ModelAdminGroup class to group several ModelAdmin classes together,
-# you only need to register the ModelAdminGroup class with Wagtail:
-modeladmin_register(MyModelAdminGroup)
+modeladmin_register(FundRaiserAdminGroup)
